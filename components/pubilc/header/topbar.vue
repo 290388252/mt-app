7752fa9f9d828d4b8147af13c4df757c08dbbf99
YChao <290388252@qq.com>
2020-02-22 09:53:29 +0800

    first commit

---
+++ components/pubilc/header/topbar.vue
@@ -0,0 +1,33 @@
+<template>
+      <el-row :gutter="0" class="m-header">
+        <el-col :span="4">
+          <geo></geo>
+        </el-col>
+        <el-col :span="6">
+        <user></user>
+        </el-col>
+        <el-col :span="14">
+          <navbar></navbar>
+        </el-col>
+      </el-row>
+</template>
+
+<script>
+  import Geo from './geo'
+  import User from './user'
+  import Navbar from './nav.vue'
+    export default {
+        components: {
+          Geo,
+          User,
+          Navbar
+        },
+        name: "index"
+    }
+</script>
+
+<style lang="scss">
+  @import "../../../assets/css/public/layout.scss";
+  @import "../../../assets/css/public/header/index.scss";
+</style>
+
